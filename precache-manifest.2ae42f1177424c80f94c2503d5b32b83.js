a87e55ee10a149a4deae608bb095fd4f3a931ba9
LodesJacob <46973657+LodesJacob@users.noreply.github.com>
2020-02-12 10:05:54 -0500

    Updates

---
+++ precache-manifest.2ae42f1177424c80f94c2503d5b32b83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec28c6ee53d73914f4c4865cabd6fdc3",
+    "url": "/dig4639-mobile-dev/index.html"
+  },
+  {
+    "revision": "c2845e3bd59addc4d091",
+    "url": "/dig4639-mobile-dev/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "e02cba94be16e740280c",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2845e3bd59addc4d091",
+    "url": "/dig4639-mobile-dev/static/js/main.f421237e.chunk.js"
+  },
+  {
+    "revision": "170169ccdc7dff24daa3",
+    "url": "/dig4639-mobile-dev/static/js/runtime-main.e7bb69b4.js"
+  }
+]);
